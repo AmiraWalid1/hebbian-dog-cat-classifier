b7c3308355107452524fc7335e62da88a062228f
radwa <radwaadel959@gmail.com>
2024-04-27 22:33:08 +0300

    guiAfterPhotos

--- index.py
+++ index.py
@@ -8,11 +8,25 @@ import webbrowser
 if __name__ == "__main__":
     window = Tk()
     window.configure(bg="#EEF8D9")
-    window.geometry("700x500")
+    window.geometry("800x500")
     window.title("Who am I 👀️")
     window.resizable(height = None , width= None)
-    HebianLabel = Label(text = "Hebbian" , fg="black" , bg = "#B4D3AC",  width="800" , font="Arial 15 bold").pack(); 
-    uploadLabel = Label(text="Upload your photo" , font="Inter 9" , fg="black" , bg ="#EEF8D9").pack(anchor="se" , side=BOTTOM , pady= 50 , padx = 20) 
-    hebbianModeButton = Button(text="Hebbian model" , fg="black" , bg="#81B774" , width=15 , font = "10").pack(side=RIGHT , padx= 190 , pady= 90)
+    window.resizable(0,0)
     
+
+    HebianLabel = Label( window , text = "Hebbian" , fg="black" , bg = "#B4D3AC",  width="800" , font="Arial 15 bold").pack()
+    hebbianModeButton = Button(window ,text="Hebbian model" , fg="black" , bg="#81B774" , width=15 , font = "10").pack(side=RIGHT , padx= 190 , pady= 90)
+    uploadLabel = Label(text="Upload your photo" , font="Inter 9" , fg="black" , bg ="#EEF8D9") .pack(anchor="n") 
+   
+
+    imgCat = Image.open('C:/Users/SOUQ COMPUTER/Downloads/cat1.png')
+    resize_image_cat = imgCat.resize((200 , 200))
+    imgC = ImageTk.PhotoImage(resize_image_cat)
+    lblCat = Label(image= imgC).pack(pady=20)
+    
+    imgDog = Image.open('C:/Users/SOUQ COMPUTER/Downloads/dog1.png')
+    resize_image_Dog = imgDog.resize((200 , 200))
+    imgD = ImageTk.PhotoImage(resize_image_Dog)
+    lblCat = Label(image= imgD).pack(pady=10)
+
     window.mainloop()
